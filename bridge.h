e8a2f51a60c7a3e271cd4d33009b850d29e50881
jmihc <john@bahai.us>
2016-09-13 11:37:53 -0500

    adding UIKit+AFNetworking, adding imageWidth

---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  photoSearchExample
+//
+//  Created by John Cornyn on 9/9/16.
+//  Copyright © 2016 Johnny Cornyn. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+#endif /* bridge_h */
--- photoSearchExample/ViewController.swift
+++ photoSearchExample/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchFlickrByHashtag("dogs")
+        
+    }
+        func searchFlickrByHashtag(searchString:String) {
+        let manager = AFHTTPSessionManager()
+            
+        let searchParameters = ["method": "flickr.photos.search",
+                                "api_key": "a9428b4f9da5e201442ccd948032ca8a",
+                                "format": "json",
+                                "nojsoncallback": 1,
+                                "text": searchString,
+                                "extras": "url_m",
+                                "per_page": 5]
+        
+        manager.GET("https://api.flickr.com/services/rest/?method=flickr.photos.search",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + responseObject.description)
+                            
+                            //let json = JSON(responseObject)
+                            //if let scroll = json["photos"][3]["url_m"].string {
+                            //}
+                            
+                            if let photos = responseObject["photos"] as? [String: AnyObject] {
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+                                    let imageWidth = self.view.frame.width
+                                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(photoArray.count))
+                                    for (i,photoDictionary) in photoArray.enumerate() {
+                                        if let imageURLString = photoDictionary["url_m"] as? String {
+                                                let imageView = UIImageView(frame: CGRectMake(0, imageWidth*CGFloat(i), imageWidth, imageWidth))
+                                                if let url = NSURL(string: imageURLString) {
+                                                    imageView.setImageWithURL(url)
+                                                    self.scrollView.addSubview(imageView)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                        }
+            },
+                    failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                        print("Error: " + error.localizedDescription)
+        })
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchFlickrByHashtag(searchText)
+        }
+    }
 
 }
 
